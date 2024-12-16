5cd1df13d84f8330e5cddd99c734e8129b7e8209
TechMustafa <150680282+TechMustafa@users.noreply.github.com>
2024-12-16 15:26:23 +0500

    scroll pagination and ui work done now

--- src/Pages/Chatbox.jsx
+++ src/Pages/Chatbox.jsx
@@ -20,6 +20,7 @@ function Chatbox({
   const [chatHistory, setChatHistory] = useState([]);
   const [socket, setSocket] = useState(null);
   const [newMessage, setNewMessage] = useState("");
+  const [pagination, setPagination] = useState({page:1,totalPages:0});
   const [time, setTime] = useState(() => {
     const currentDate = new Date();
     const hours = currentDate.getHours().toString().padStart(2, "0");
@@ -28,10 +29,13 @@ function Chatbox({
   });
   const [errors, seterror] = useState("");
   const navigate = useNavigate();
+  
 
   // const SOCKET_SERVER_URL = `http://192.168.18.200:4001`;
   const SOCKET_SERVER_URL = "https://api.ahle.chat";
   const lastMessageRef = useRef(null);
+  const chatContainerRef = useRef(null);
+
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
@@ -41,11 +45,12 @@ function Chatbox({
     }
     const url = `${SOCKET_SERVER_URL}/api/chats/history?otherUserEmail=${groupIds}&userEmail=${
       "sender@gmail.com" || agentEmail
-    }&page=1&perPage=50`;
+    }&page=${pagination?.page}&perPage=10`;
 
     try {
       const res = await axios.get(url);
-      setChatHistory(res.data.messages);
+      setChatHistory((prevHistory) => [...prevHistory, ...res.data.messages]);
+      setPagination({...pagination,totalPages:res.data.totalPages})
     } catch (error) {
       const errorMessage =
         error?.response?.data?.message ||
@@ -142,6 +147,23 @@ function Chatbox({
     }
   }, [chatHistory]);
 
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }
+  }, [chatHistory]);
+
+
+ const handleScroll = () => {
+    const container = chatContainerRef.current;
+
+    if (container.scrollTop === 0 && pagination.totalPages > pagination.page) {      
+      setPagination({...pagination,page:pagination.page ++ })
+      console.log("Checkfirst",pagination)
+      fetchChatHistory()
+    }
+  };
+
   return (
     <div className="w-full ">
       <div className="flex justify-between items-center border-b-2 p-3 border-gray-200">
@@ -191,119 +213,115 @@ function Chatbox({
         )}
       </div>
       <div className="flex flex-col space-y-4 mb-4 max-h-[500px] overflow-y-auto py-5 px-3">
-        {groupIds ? (
-          <>
-            {chatHistory && chatHistory?.length > 0 ? (
-              <div className="flex flex-col flex-grow overflow-y-auto max-h-[97%]">
-                {chatHistory?.map((msg, index) => {
-                  const messageDate = new Date(msg?.timestamp);
-                  const formattedMessageDate = formatMessageDate(messageDate);
+      {groupIds ? (
+        <>
+          {chatHistory && chatHistory?.length > 0 ? (
+            <div
+              ref={chatContainerRef}
+              onScroll={handleScroll}
+              className="flex flex-col flex-grow overflow-y-auto max-h-[97%] thin-scrollbar no-scrollbar-arrows"
+            >
+              {chatHistory?.map((msg, index) => {
+                const messageDate = new Date(msg?.timestamp);
+                const formattedMessageDate = formatMessageDate(messageDate);
 
-                  const prevMsgDate =
-                    index > 0
-                      ? new Date(chatHistory[index - 1]?.timestamp)
-                      : null;
-                  const isDifferentDay =
-                    prevMsgDate &&
-                    messageDate.toDateString() !== prevMsgDate.toDateString();
+                const prevMsgDate =
+                  index > 0
+                    ? new Date(chatHistory[index - 1]?.timestamp)
+                    : null;
+                const isDifferentDay =
+                  prevMsgDate &&
+                  messageDate.toDateString() !== prevMsgDate.toDateString();
 
-                  return (
-                    <div key={index}>
-                      {/* Display Date Header */}
-                      {index === 0 || isDifferentDay ? (
-                        <div className="flex justify-center my-8">
-                          <h6 className="p-1 rounded-md text-white">
-                            {formattedMessageDate}
-                          </h6>
-                        </div>
-                      ) : null}
-
-                      {/* Message Bubble */}
-                      <div
-                        className={`flex ${
-                          msg?.senderEmail === "sender@gmail.com"
-                            ? "justify-end"
-                            : "justify-start"
-                        } mb-2`}
-                      >
-                        {/* Sender's Message */}
-                        {msg?.senderEmail === "sender@gmail.com" && (
-                          <div className="flex items-end max-w-[70%] mr-2">
-                            {/* <div className="text-[white] bg-[#4CAF50] rounded-l-xl rounded-br-xl py-2 px-3"> */}
-                            <div className="text-black bg-[#f7f7f7] rounded-r-xl rounded-bl-xl py-2 px-3">
-                              <div className="text-sm">{msg?.message}</div>
-                            </div>
-                            <div
-                              className="text-xs text-[#CDD1ce] mr-2 mx-2 mb-5"
-                              style={{ textAlign: "end" }}
-                            >
-                              {msg?.timestamp
-                                ? extractTimeFromTimestamp(msg?.timestamp)
-                                : time}
-                            </div>
-                          </div>
-                        )}
-
-                        {/* Receiver's Message */}
-                        {msg?.senderEmail !== "sender@gmail.com" && (
-                          <div className="flex items-start max-w-[70%] ml-2">
-                            {/* <div className="text-[white] bg-[#19335F] rounded-r-xl rounded-bl-xl py-2 px-3"> */}
-                            <div className="text-[white] bg-[#0496ff] rounded-l-xl rounded-br-xl py-2 px-3">
-                              <div className="text-sm">{msg?.message}</div>
-                            </div>
-                            <div
-                              className="text-xs text-[#CDD1ce] ml-2"
-                              style={{ textAlign: "start" }}
-                            >
-                              {msg?.timestamp
-                                ? extractTimeFromTimestamp(msg?.timestamp)
-                                : time}
-                            </div>
-                          </div>
-                        )}
+                return (
+                  <div key={index}>
+                    {/* Display Date Header */}
+                    {index === 0 || isDifferentDay ? (
+                      <div className="flex justify-center my-8">
+                        <h6 className="p-1 rounded-md text-white">
+                          {formattedMessageDate}
+                        </h6>
                       </div>
+                    ) : null}
 
-                      {/* Scroll to last message */}
-                      {index === chatHistory?.length - 1 && (
-                        <div ref={lastMessageRef} />
+                    {/* Message Bubble */}
+                    <div
+                      className={`flex ${
+                        msg?.senderEmail === "sender@gmail.com"
+                          ? "justify-end"
+                          : "justify-start"
+                      } mb-2`}
+                    >
+                      {/* Sender's Message */}
+                      {msg?.senderEmail === "sender@gmail.com" && (
+                        <div className="flex items-end max-w-[70%] mr-2">
+                          <div className="text-black bg-[#f7f7f7] rounded-r-xl rounded-bl-xl py-2 px-3">
+                            <div className="text-sm">{msg?.message}</div>
+                          </div>
+                          <div
+                            className="text-xs text-[#CDD1ce] mr-2 mx-2 mb-5"
+                            style={{ textAlign: "end" }}
+                          >
+                            {msg?.timestamp
+                              ? extractTimeFromTimestamp(msg?.timestamp)
+                              : ""}
+                          </div>
+                        </div>
+                      )}
+
+                      {/* Receiver's Message */}
+                      {msg?.senderEmail !== "sender@gmail.com" && (
+                        <div className="flex items-start max-w-[70%] ml-2">
+                          <div className="text-[white] bg-[#0496ff] rounded-l-xl rounded-br-xl py-2 px-3">
+                            <div className="text-sm">{msg?.message}</div>
+                          </div>
+                          <div
+                            className="text-xs text-[#CDD1ce] ml-2"
+                            style={{ textAlign: "start" }}
+                          >
+                            {msg?.timestamp
+                              ? extractTimeFromTimestamp(msg?.timestamp)
+                              : ""}
+                          </div>
+                        </div>
                       )}
                     </div>
-                  );
-                })}
-              </div>
-            ) : (
-              <div className="flex justify-center items-center h-full">
-                <p className="text-white text-lg">No Message Found</p>
-              </div>
-            )}
-
-            {/* Message Input */}
-            <div className="flex justify-between items-center p-4 bg-[#323234] absolute rounded-[10px] bottom-0 w-[98%]">
-              <input
-                type="text"
-                value={newMessage}
-                disabled={errors}
-                onChange={(e) => setNewMessage(e.target.value)}
-                placeholder="Type your message..."
-                className="flex-grow px-4 py-2 rounded-l-lg border bg-white text-black border-gray-300 focus:outline-none"
-                onKeyPress={(e) => {
-                  if (e.key === "Enter") handleSendMessage();
-                }}
-              />
-              <button
-                onClick={handleSendMessage}
-                className="flex items-center justify-center px-4 pt-[0.8rem] pb-[0.8rem] text-white rounded-r-lg bg-white transition"
-              >
-                <FaPaperPlane className="text-blue-500" />
-              </button>
+                  </div>
+                );
+              })}
             </div>
-          </>
-        ) : (
-          <div className="flex justify-center items-center h-screen">
-            <p className="text-white text-lg">Let's Start Chat</p>
+          ) : (
+            <div className="flex justify-center items-center h-full">
+              <p className="text-white text-lg">No Message Found</p>
+            </div>
+          )}
+
+          {/* Message Input */}
+          <div className="flex justify-between items-center p-4 bg-[#323234] absolute rounded-[10px] bottom-0 w-[98%]">
+            <input
+              type="text"
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              placeholder="Type your message..."
+              className="flex-grow px-4 py-2 rounded-l-lg border bg-white text-black border-gray-300 focus:outline-none"
+              onKeyPress={(e) => {
+                if (e.key === "Enter") handleSendMessage();
+              }}
+            />
+            <button
+              onClick={handleSendMessage}
+              className="flex items-center justify-center px-4 pt-[0.8rem] pb-[0.8rem] text-white rounded-r-lg bg-white transition"
+            >
+              <FaPaperPlane className="text-blue-500" />
+            </button>
           </div>
-        )}
-      </div>
+        </>
+      ) : (
+        <div className="flex justify-center items-center h-screen">
+          <p className="text-white text-lg">Let's Start Chat</p>
+        </div>
+      )}
+    </div>
     </div>
   );
 }
